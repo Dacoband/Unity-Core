4d614a644264685915858645fdd200a98f6fedb3
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2024-09-09 18:23:10 +0700

    Add PipeSpawner  create function

--- demo/Assets/BirdScript.cs
+++ demo/Assets/BirdScript.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +6,7 @@ public class BirdScript : MonoBehaviour
 {
     public float flapStrenght;
     public Rigidbody2D myRigidbody;
+    public PipeSpawner pipeSpawner;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,10 +16,20 @@ public class BirdScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow)) 
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow))
         {
             myRigidbody.velocity = Vector2.up * 10;
             myRigidbody.velocity = Vector2.up * flapStrenght;
+
+
+            if (pipeSpawner != null)
+            {
+                pipeSpawner.SpawnPipe();
+            }
+            else
+            {
+                Debug.LogError("PipeSpawner chưa được gán!");  // Thông báo lỗi trong Console nếu chưa gán      
+            }
         }
     }
 }
---
+++ demo/Assets/PipeSpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSpawner : MonoBehaviour
+{
+    public GameObject pipePrefab;  // Đối tượng Prefab của Pipe
+    public float spawnInterval = 2f;  // Thời gian giữa các lần sinh Pipe
+    public float pipeHeightOffset = 2f;  // Độ lệch cao độ của Pipe
+
+    private float timeSinceLastSpawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeSinceLastSpawn = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Kiểm tra nếu đã đủ thời gian để sinh thêm pipe
+        if (timeSinceLastSpawn >= spawnInterval)
+        {
+            SpawnPipe();
+            timeSinceLastSpawn = 0f;  // Reset lại thời gian
+        }
+        else
+        {
+            timeSinceLastSpawn += Time.deltaTime;
+        }
+    }
+
+    public void SpawnPipe()
+    {
+        // Tạo một vị trí ngẫu nhiên cho Pipe mới
+        float randomHeight = Random.Range(-pipeHeightOffset, pipeHeightOffset);
+        Vector3 spawnPosition = new Vector3(transform.position.x, randomHeight, 0);
+
+        // Sinh ra Pipe mới
+        Instantiate(pipePrefab, spawnPosition, Quaternion.identity);
+    }
+}
