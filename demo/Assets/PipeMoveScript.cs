8b2bddd3372ebe96761089f22fc7d6153c4a1e14
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2024-10-10 17:05:55 +0700

    move speed

--- demo/Assets/PipeMoveScript.cs
+++ demo/Assets/PipeMoveScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PipeMoveScript : MonoBehaviour
 {
-    public float moveSpeed = 0.5f;
+    public float moveSpeed = 0.001f;
     // Start is called before the first frame update
     void Start()
     {
